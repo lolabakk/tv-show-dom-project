d121ecd5faf84b9b3e8c12804a06084640bf81da
Lola Bello <mololabello@gmail.com>
2020-09-05 23:32:31 +0100

    select

--- script.js
+++ script.js
@@ -3,13 +3,14 @@ function setup() {
   const allEpisodes = getAllEpisodes();
   console.log(allEpisodes);
   makePageForEpisodes(allEpisodes);
+
 }
 // the episode 's name
 // the season number the episode number the episode 's medium-sized image
 // the episode 's summary text 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-   let container = document.querySelector(".container");
+  let container = document.querySelector(".container");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
   episodeList.forEach(function (episode) {
     let episodeContainer = document.createElement("div");
@@ -19,7 +20,7 @@ function makePageForEpisodes(episodeList) {
     let headerEpisode = document.createElement("h3");
     let imageEpisode = document.createElement("img");
     let episodeSummary = document.createElement("p");
-    // let searchEpisode = document.createElement("search");
+
     // S02E07
     headerEpisode.textContent = `${episode.name}-${episodeCode}`;
 
@@ -33,17 +34,39 @@ function makePageForEpisodes(episodeList) {
     episodeContainer.style.border = "5px solid pink";
     episodeContainer.className = "epiContainer";
     rootElem.className = "rootContainer";
-
-
-
-
-
-
-
-
   })
+  //  for loop
+  // episodeList.forEach(function (episode) {
+
+  //   let OptionSelector = document.getElementById("episodeSelection")
+  OptionSelector= document.createElement('option');
 
 
+// create new option element
+
+ 
+
+    // let episodeSelector = document.getElementById("episodeSelector").selectedIndex;
+    // let episodeOption = document.getElementsByi("episodeOption")[episodeSelector].value);
+  
+
+  
+
+  // S02E07
 }
+let searchBox = document.getElementById("searchBox");
+const allEpisodes = getAllEpisodes();
+const rootElem = document.getElementById("root");
+searchBox.addEventListener("keyup", function (event) {
+  searchBox = event.target.value.toLowerCase();
+  let newArray = allEpisodes.filter(function (item) {
+    return (
+      item.name.toLowerCase().includes(searchBox) ||
+      item.summary.toLowerCase().includes(searchBox)
+    );
+  });
 
+  rootElem.textContent = "";
+  makePageForEpisodes(newArray);
+});
 window.onload = setup;
